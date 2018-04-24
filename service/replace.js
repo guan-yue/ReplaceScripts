3116cd32ba487fdf9271ec6478eb8e3d7c6c2bc7
guanyue <1853376164@qq.com>
2018-04-24 15:24:00 +0800

    Add files via upload

--- service/replace.js
+++ service/replace.js
@@ -1,1 +1,11 @@
-
+chrome.webRequest.onBeforeRequest.addListener(
+//chrome.webRequest.onHeadersReceived.addListener(
+	function(details){
+		return {redirectUrl: chrome.extension.getURL("music163core.js")};
+	},
+	{
+	urls:["https://s3.music.126.net/web/s/core.js*"],
+	types:["script"]
+	},
+	["blocking"]
+);
