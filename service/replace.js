7f8b8a298d8c2739dc117977f21ffd72bfeeb6ef
guanyue <1853376164@qq.com>
2018-04-27 15:09:29 +0800

    Update replace.js

--- service/replace.js
+++ service/replace.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
 //chrome.webRequest.onHeadersReceived.addListener(
 	function(details){
-		return {redirectUrl: chrome.extension.getURL("music163core.js")};
+		return {redirectUrl: chrome.extension.getURL("music163core.js")};//将请求重定向
 	},
 	{
 	urls:["https://s3.music.126.net/web/s/core.js*"],
